4fbbfa26e23d976950970e5254a82bd495085cfb
Jonathan B <bluxget@gmail.com>
2018-10-01 10:59:06 +0200

    Average positive list function

---
+++ S1_algotools.py
@@ -0,0 +1,32 @@
+def average_above_zero(tab):
+	"""
+	brief: computes ten average of the list 
+	args:
+		tab: a list of numeric value expects at leas one positive value
+	return:
+		the computed average
+	raises:
+		ValueError if expected a list as input
+		ValueError if no positive value found
+	"""
+	if not(isinstance(tab, list)):
+		raise ValueError('Expected a list as input')
+
+	valSum = 0.0
+	nPositiveValues = 0
+
+	for i in range(len(tab)):
+		if tab[i] > 0:
+			valSum += float(tab[i])
+			nPositiveValues += 1
+
+	if nPositiveValues <= 0:
+		raise ValueError('No positive value found')
+	
+	avg = valSum/nPositiveValues
+
+	return avg
+
+notes = [12, 0, 20, -5]
+
+print(average_above_zero(notes))
