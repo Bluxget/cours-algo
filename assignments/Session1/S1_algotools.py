b7c4eb3cd6d9b419902fe8e312dd9b8e7e9144e0
Jonathan B <bluxget@gmail.com>
2018-10-08 14:35:09 +0200

    Unit test

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -27,31 +27,38 @@ def average_above_zero(tab):
 
 	return avg
 
+def test_average_above_zero():
+	assert average_above_zero([12, 0, 20, -5]) == 17.0
+
 def maximum_value(tab):
 	"""
 	brief: return maximum value of the list
 	args:
 		tab: a list of numeric value expects at leas one positive value
 	return:
-		the computed average
+		the max value of the list
+		the index of the max value
 	raises:
 		ValueError if expected a list as input
+		ValueError if no positive value found
 	"""
 	if not(isinstance(tab, list)):
 		raise ValueError('Expected a list as input')
 
-	valMax = 0
+	valMax = 0.0
+	valMaxIndex = -1;
 	nPositiveValues = 0
 
 	for i in range(len(tab)):
 		if tab[i] >= 0 and tab[i] > valMax:
-			valMax = tab[i]
+			valMax = float(tab[i])
+			valMaxIndex = i
 			nPositiveValues += 1
 
 	if nPositiveValues <= 0:
 		raise ValueError('No positive value found')
 
-	return valMax
+	return valMax, valMaxIndex
 
 notes = [12, 0, 20, -5]
 
