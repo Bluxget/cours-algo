3cc819dd7b3b2613765196fb82ae545cbfe6405a
Jonathan B <bluxget@gmail.com>
2018-10-08 14:16:32 +0200

    Table maximum value in progress

--- S1_algotools.py
+++ S1_algotools.py
@@ -1,6 +1,6 @@
 def average_above_zero(tab):
 	"""
-	brief: computes ten average of the list 
+	brief: computes ten average of the list
 	args:
 		tab: a list of numeric value expects at leas one positive value
 	return:
@@ -27,6 +27,33 @@ def average_above_zero(tab):
 
 	return avg
 
+def maximum_value(tab):
+	"""
+	brief: return maximum value of the list
+	args:
+		tab: a list of numeric value expects at leas one positive value
+	return:
+		the computed average
+	raises:
+		ValueError if expected a list as input
+	"""
+	if not(isinstance(tab, list)):
+		raise ValueError('Expected a list as input')
+
+	valMax = 0
+	nPositiveValues = 0
+
+	for i in range(len(tab)):
+		if tab[i] >= 0 and tab[i] > valMax:
+			valMax = tab[i]
+			nPositiveValues += 1
+
+	if nPositiveValues <= 0:
+		raise ValueError('No positive value found')
+
+	return valMax
+
 notes = [12, 0, 20, -5]
 
 print(average_above_zero(notes))
+print(maximum_value(notes))
